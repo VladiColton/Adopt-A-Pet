21f60235bdc1cf465d3f159c35fee6e51884356c
erez <erezmoritz1@gmail.com>
2018-04-02 17:44:25 +0300

    changed to hiberante jpa provider + animalService class

--- src/java/service/AnimalService.java
+++ src/java/service/AnimalService.java
@@ -1,16 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package service;
 
-//import javax.persistence.*;
+import javax.persistence.*;
+import entities.Animal;
 
 public class AnimalService {
-//    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("Adopt-A-PetPU");
-//    private EntityManager em = emf.createEntityManager();
-//    private EntityTransaction tx = em.getTransaction();
-//    
-//    public Anima
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("Adopt-A-PetPU");
+    private EntityManager em = emf.createEntityManager();
+    private EntityTransaction tx = em.getTransaction();
+    
+    public Animal createAnimal(Animal animal) {
+        tx.begin();
+        em.persist(animal);
+        tx.commit();
+        return animal;
+    }
+    
+    public Animal findAnimal(long id) {
+        return em.find(Animal.class, id);
+    }
+    
+    public void removeAnimal(long id) {
+        Animal animal = findAnimal(id);
+        if (animal != null) {
+            tx.begin();
+            em.remove(animal);
+            tx.commit();
+        }
+    }
+    //TODO - updating animal, can be done directly with JPQL
 }
