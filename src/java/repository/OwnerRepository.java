cc8879647f103b3fbc0d398c56aa7c36e8cb947e
erez <erezmoritz1@gmail.com>
2018-04-14 10:13:32 +0300

    added animal repository + generic + update entity capability

---
+++ src/java/repository/OwnerRepository.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repository;
+
+import entities.Owner;
+
+public class OwnerRepository extends Repository<Owner> {
+
+    public OwnerRepository() {
+        super(Owner.class);
+    }
+}
---
+++ src/java/repository/Repository.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repository;
+
+import javax.persistence.*;
+import entities.Persistable;
+
+public abstract class Repository<T extends Persistable> {
+
+    private final EntityManagerFactory emf
+            = Persistence.createEntityManagerFactory("Adopt-A-PetPU");
+    private final EntityManager em = emf.createEntityManager();
+    private final EntityTransaction tx = em.getTransaction();
+
+    private final Class<T> type;
+
+    public Repository(Class<T> type) {
+        this.type = type;
+    }
+
+    public T create(T t) {
+        tx.begin();
+        em.persist(t);
+        tx.commit();
+        return t;
+    }
+
+    public T find(long id) {
+        return em.find(type, id);
+    }
+
+    public void remove(long id) {
+        T t = find(id);
+        if (t != null) {
+            tx.begin();
+            em.remove(t);
+            tx.commit();
+        }
+    }
+
+    public void update(T t) {
+        Long id = t.getId();
+        if (find(id) == null) {
+            throw new EntityNotFoundException("entity with id " + id + " does not exist");
+        }
+        tx.begin();
+        em.merge(t);
+        tx.commit();
+    }
+}
