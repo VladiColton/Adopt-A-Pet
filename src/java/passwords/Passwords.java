cb8f26efac2388fd541ba3377210c692983f2e71
erez <erezmoritz1@gmail.com>
2018-04-18 12:37:39 +0300

    add new passwords path

---
+++ src/java/passwords/Passwords.java
@@ -0,0 +1,20 @@
+package passwords;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * Password hashing and validation helper class
+ */
+public class Passwords {
+
+    private static final int WORKLOAD = 15;
+
+    public static String hashPassword(String plainPassword) {
+        String salt = BCrypt.gensalt(WORKLOAD);
+        return BCrypt.hashpw(plainPassword, salt);
+    }
+
+    public static boolean checkPassword(String plainPassword, String storedHash) {
+        return BCrypt.checkpw(plainPassword, storedHash);
+    }
+}
