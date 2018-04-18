08b65e329a58af3692adde69d92111c9991e1411
erez <erezmoritz1@gmail.com>
2018-04-18 12:45:18 +0300

    remove junk

--- src/java/mainmanager/MainPageMGR.java
+++ src/java/mainmanager/MainPageMGR.java
@@ -6,28 +6,23 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.*;
-import repository.OwnerRepository;
+
 /**
  * Main Page manager class before user login
+ *
  * @author Vladi Colton
  */
 @RequestScoped
-@ManagedBean (name = "mainPageMGR", eager = true)
+@ManagedBean(name = "mainPageMGR", eager = true)
 public class MainPageMGR implements Serializable {
+
     //private LinkedList<Animal> animals;
     private List<Animal> _animals;
-    public MainPageMGR()
-    {
-        Owner o = Owner.builder().email("wowthis").password("wowthis").name("cvadf").build();
-        OwnerRepository r = new OwnerRepository();
-        r.create(o);
-        Owner x = r.find(o.getId());
-        r.isEmailAvailable("wowthis1");
-        r.isEmailAvailable("wowthis");
-        
+
+    public MainPageMGR() {
         // TODO - remove file
         this._animals = new ArrayList<>();
-        
+
         this._animals.add(new Animal(Owner.builder().name("Sivan" + " " + "Schrier").location("Eilat" + ", " + "Israel").phoneNumber(546903018).email("Sivan@sivan.com").build(), "testAnimal", "type1", "subtype", "Animal_01", 10));
         this._animals.add(new Animal(Owner.builder().name("Vladi" + " " + "Colton").location("Nesher" + ", " + "Israel").phoneNumber(546903018).email("Vladi@Vladi.com").build(), "testAnima2", "type2", "subtype", "Animal_02", 12));
         this._animals.add(new Animal(null, "testAnimal", "type1", "subtype", "Animal_03", 10));
@@ -35,15 +30,13 @@ public class MainPageMGR implements Serializable {
         this._animals.add(new Animal(null, "testAnimal", "type1", "subtype", "Animal_05", 10));
         this._animals.add(new Animal(null, "testAnimal", "type1", "subtype", "Animal_06", 10));
     }
-    
-    public String getPets()
-    {
+
+    public String getPets() {
         return this._animals.get(0).getName();
     }
-    
-    public List<Animal> getAnimals()
-    {
+
+    public List<Animal> getAnimals() {
         return this._animals;
     }
-    
+
 }
