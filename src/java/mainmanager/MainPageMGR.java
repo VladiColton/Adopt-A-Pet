074579c168e4eeb44dde3f0250acef93c863f107
erez <erezmoritz1@gmail.com>
2018-04-22 19:40:30 +0300

    refactor animal, add builder & new fields

--- src/java/mainmanager/MainPageMGR.java
+++ src/java/mainmanager/MainPageMGR.java
@@ -26,69 +26,60 @@ public class MainPageMGR implements Serializable {
     private String animalTypeToView;
     private int imageIndex;
     private List<Animal> listOfAnimals;
-    
-    
+
     public MainPageMGR() {
         animalTypeToView = "all";
         imageIndex = -1;
-        
+
         byte[] animalProfPic = null;
-        try
-        {
+        try {
             //Get directory with default image
             File f = new File(UploadController.class.getProtectionDomain().getCodeSource().getLocation().getPath());
             String imageLocation = f.getPath(); //Set relative path to the DB
-            imageLocation = imageLocation.substring(0, imageLocation.indexOf("Adopt-A-Pet")+12) + "\\web\\images";
+            imageLocation = imageLocation.substring(0, imageLocation.indexOf("Adopt-A-Pet") + 12) + "\\web\\images";
             //Get default image
             File usrImageFile = new File(imageLocation + "\\DefaultAnimalProfileImage.png");
             //Create byte array for saving in the DB
             animalProfPic = Files.readAllBytes(usrImageFile.toPath());
-        }
-        catch (IOException ex) 
-        {
+        } catch (IOException ex) {
             ex.printStackTrace(System.out);
         }
-        
+
         //Create animals
         this._animals = new ArrayList<>();
-        
-        Animal an1 = new Animal(Owner.builder().name("Sivan" + " " + "Schrier").location("Eilat" + ", " + "Israel").phoneNumber(546903018).email("Sivan@sivan.com").build(), "testAnimal", "dog", "subtype", "Animal_01", 10);
-        an1.setAnimalPic(animalProfPic);
-        this._animals.add(an1);
-        Animal an2 = new Animal(Owner.builder().name("Vladi" + " " + "Colton").location("Nesher" + ", " + "Israel").phoneNumber(546903018).email("Vladi@Vladi.com").build(), "testAnima2", "cat", "subtype", "Animal_02", 12);
-        an2.setAnimalPic(animalProfPic);
-        this._animals.add(an2);
-        Animal an3 = new Animal(null, "testAnimal desk3", "type3", "subtype2", "Animal_03", 16);
-        an3.setAnimalPic(animalProfPic);
-        this._animals.add(an3);
-        Animal an4 = new Animal(null, "testAnimal desk4", "type4", "subtype3", "Animal_04", 177);
-        an4.setAnimalPic(animalProfPic);
-        this._animals.add(an4);
+
+        // Use new builder
+//        Animal an1 = new Animal(Owner.builder().name("Sivan" + " " + "Schrier").location("Eilat" + ", " + "Israel").phoneNumber(546903018).email("Sivan@sivan.com").build(), "testAnimal", "dog", "subtype", "Animal_01", 10);
+//        an1.setAnimalPic(animalProfPic);
+//        this._animals.add(an1);
+//        Animal an2 = new Animal(Owner.builder().name("Vladi" + " " + "Colton").location("Nesher" + ", " + "Israel").phoneNumber(546903018).email("Vladi@Vladi.com").build(), "testAnima2", "cat", "subtype", "Animal_02", 12);
+//        an2.setAnimalPic(animalProfPic);
+//        this._animals.add(an2);
+//        Animal an3 = new Animal(null, "testAnimal desk3", "type3", "subtype2", "Animal_03", 16);
+//        an3.setAnimalPic(animalProfPic);
+//        this._animals.add(an3);
+//        Animal an4 = new Animal(null, "testAnimal desk4", "type4", "subtype3", "Animal_04", 177);
+//        an4.setAnimalPic(animalProfPic);
+//        this._animals.add(an4);
     }
-    
-    public List<Animal> getAnimals() 
-    {
+
+    public List<Animal> getAnimals() {
         List<Animal> res;
-        if (animalTypeToView.equalsIgnoreCase("all"))
-        {
+        if (animalTypeToView.equalsIgnoreCase("all")) {
             res = this._animals;
-        }
-        else
-        {
+        } else {
             //Build new sublist to return with only selected animals
             res = this._animals.stream().filter(a -> a.getType().equalsIgnoreCase(animalTypeToView)).collect(Collectors.toList());
         }
-        
+
         //Before exit update the session with active list
         setAnimalsList(res);
         return res;
     }
-    
-    public void setAnimalFilter(String animalType)
-    {
+
+    public void setAnimalFilter(String animalType) {
         //Switch case
-        switch(animalType)
-        {
+        switch (animalType) {
             case "dog":
             case "cat":
             case "parrot":
@@ -99,18 +90,17 @@ public class MainPageMGR implements Serializable {
                 break;
         }
     }
-    
-    public void setAnimalsList(List<Animal> list)
-    {
+
+    public void setAnimalsList(List<Animal> list) {
         imageIndex = -1;
         listOfAnimals = list;
     }
-    
-    public StreamedContent getAnimalImageFromDB()
-    {
+
+    public StreamedContent getAnimalImageFromDB() {
         imageIndex++;
-        if(listOfAnimals.size() == imageIndex)
-            imageIndex = imageIndex-1;
+        if (listOfAnimals.size() == imageIndex) {
+            imageIndex = imageIndex - 1;
+        }
         return new DefaultStreamedContent(new ByteArrayInputStream(listOfAnimals.get(imageIndex).getAnimalPic()));
     }
 }
